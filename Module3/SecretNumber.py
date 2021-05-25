752ebf972fedc56d19518137a2a799243418c1e0
elliottgreen <omar.elliottgreen1@ieee.org>
2021-05-25 11:22:08 -0700

    @fix: Loop fixed. Not full solution.

--- Module3/SecretNumber.py
+++ Module3/SecretNumber.py
@@ -1,7 +1,7 @@
 secret_number = 777
 
- print(
-         """
+print(
+        ''' 
          +================================+
          | Welcome to my game, muggle!    |
          | Enter an integer number        |
@@ -9,16 +9,17 @@ secret_number = 777
          | picked for you.                |
          | So, what is the secret number? |
          +================================+
-         """)
+        ''') 
 
- number = int(input("Guess my number or type 0 to give up: "))
-
+number = 9999
 # 0 terminates execution.
 while number != 0:
+    number = int(input("Guess my number or type 0 to give up: "))
     if number == secret_number:
-        number = secret_number
-        print ("Well done, muggle! You are free now.")
+       #number = secret_number
+       print ("Well done, muggle! You are free now.")
+       break
     else:
         print ("Ha ha! You're stuck in my loop!")
 
-number = int(input("Guess my number or type 0 to give up: "))
+# number = int(input("Guess my number or type 0 to give up: "))
